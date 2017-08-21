05f1b63fb50a401a284bb19aa53954f09642986f
Important Yogurt <mrska552@gmail.com>
2017-08-21 18:17:52 -0500

    Now converts to ascii...backwards
    Creates ascii image but flipped upside down

--- image.c
+++ image.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 //https://en.wikipedia.org/wiki/BMP_file_format
 
-#define max 54+(256*256) //Maximum filename size
+#define max 3*(256*256)-1 //Maximum filename size
 const char no_file[] = "File does not exist";
 const char error[] = "Invalid arguments, run file with no arguments for help\n";
 const char help[] = "./image <filename>\n";
@@ -16,15 +17,24 @@ const char wrong_bit[] = "This program only supports 24-bit bitmap as of now\n";
     2 : Overflow probably
 */
 
+/*Colors
+    0 :  (space)
+    1 : *
+    2 : #
+    3 : $
+*/
+
 
 int main(int argc, char *argv[]){
 
-    FILE *image;
+    FILE *image, *dump;
     signed int width, height;
     int c;
-    int i = 0, f = 0, a = 54, x=0;
+    int i = 0, f = 0, a = 54, x;
 	char *buffer; // Buffer for the hex to dec
 	int image_size;
+	char txt[] = "txt";
+	int avg;
 	
 	unsigned char red, green, blue;
     
@@ -42,6 +52,14 @@ int main(int argc, char *argv[]){
     
     image = fopen(argv[1], "r");
     
+    dump = fopen("ascii.txt", "a");
+    if(dump == NULL){
+        dump = fopen("ascii.txt", "w");
+    }
+    fprintf(dump, "\n\n%s\n", argv[1]);
+    
+    //just add this ascii art to the end of the file and put the filename in front of it
+    
     if(image){
         
         fseek(image, 0, SEEK_END);
@@ -56,7 +74,7 @@ int main(int argc, char *argv[]){
         
         fseek(image, 0, SEEK_SET);//Back to the start
         
-        //Only feed this images that are less than 256 X 256
+        //Only feed images that are less than 256 X 256
         fseek(image, 18 , 0); //get width
         width = fgetc(image);
         
@@ -75,24 +93,34 @@ int main(int argc, char *argv[]){
         
         fseek(image, a, 0);
         
-        while (x < height*width){                 //(c = getc(image)) != EOF){
+        for(x=0; x < height*width; x++){                 //(c = getc(image)) != EOF){
             red = fgetc(image);
             blue = fgetc(image);
             green = fgetc(image);
             
-            printf("[%d,%d]  R : %d, G : %d, B : %d\n", f, i, red, blue, green);
+            //printf("[%d,%d]  R : %d, G : %d, B : %d\n", f, i, red, blue, green);
+            
+            avg = ((int)red + (int)blue-1 + (int)green)/3;
+            
+            if(avg < 51){
+                fprintf(dump, "#");
+            }else if((avg >= 51) && (avg < 102)){
+                fprintf(dump, "*");
+            }else if((avg >= 102) && (avg < 153)){
+                fprintf(dump, "^");
+            }else if((avg >= 153) && (avg < 204)){
+                fprintf(dump, "-");
+            }else{
+                fprintf(dump, " ");
+            }
 
             if(i == width-1){
-                printf("----Line Break----\n");
                 i = 0;
                 f++;
-                fgetc(image);
-                fgetc(image);
-                //fix the padding issue
+                fprintf(dump, " BR \n");
             }else{
                 i++;
             }
-            x++;
         }
     
     
@@ -100,6 +128,7 @@ int main(int argc, char *argv[]){
         free(buffer);
         printf("\n");
         fclose(image);
+        fclose(dump);
         return 0;
     }else{
         printf("%s\n",no_file);
